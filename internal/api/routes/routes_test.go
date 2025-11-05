cb4ab064aedbc761e95971717e29e5aed0c26190
João Victor <joaovictordsantosr@gmail.com>
2025-11-05 15:01:08 -0300

    test: add route registration validation tests

---
+++ internal/api/routes/routes_test.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestInitRoutes(t *testing.T) {
+	InitRoutes()
+
+	routes := []string{"/", "/create", "/do/", "/remove/", "/list"}
+	for _, route := range routes {
+		_, pattern := http.DefaultServeMux.Handler(&http.Request{URL: &url.URL{Path: route}})
+		if pattern == "" {
+			t.Errorf("Route %s not registered", route)
+		}
+	}
+}
