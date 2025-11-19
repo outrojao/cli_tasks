1dba62c215b38ef9a6116ac37ab45856fedc6786
João Victor <joaovictordsantosr@gmail.com>
2025-11-18 22:19:36 -0300

    feat: add .env file loading with error handling in main function

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,16 @@ import (
 	"cli_tasks/internal/client"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load("configs/.env"); err != nil {
+		log.Println("Error loading .env file:", err)
+		return
+	}
+
 	if !client.HealthCheck() {
 		log.Fatal("API health check failed")
 		os.Exit(1)
